3c9f5810f4681e349a92e4ae13d43c52d5a5776b
Filip Pyda <filip.pyda@gmail.com>
2012-06-02 10:31:08 +0200

    Wait 7 instead of 3 days before deleting unconfirmed games

--- config/initializers/application_variables.rb
+++ config/initializers/application_variables.rb
@@ -1,3 +1,3 @@
-Ladder8::Application.config.auto_delete     = 3
+Ladder8::Application.config.auto_delete     = 7
 Ladder8::Application.config.ban_rmp         = true
 Ladder8::Application.config.wesnoth_version = '1.10'
